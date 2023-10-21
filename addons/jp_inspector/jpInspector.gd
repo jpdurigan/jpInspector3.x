e0a974c61d02ff76e4c4e1a2c771e4215ebdf2e6
jp durigan <jpdurigan@gmail.com>
2023-10-21 12:55:57 -0300

    add custom title to category

--- addons/jp_inspector/jpInspector.gd
+++ addons/jp_inspector/jpInspector.gd
@@ -2,11 +2,61 @@ extends Reference
 class_name jpInspector
 
 const CATEGORY_PREFIX = "_c_"
+const CATEGORY_KEY_TITLE = "title"
+const CATEGORY_KEY_ICON = "icon"
+
+
+static func Category(title: String = "", icon: String = "") -> Dictionary:
+	return {
+		CATEGORY_KEY_TITLE: title,
+		CATEGORY_KEY_ICON: icon,
+	}
+
 
 static func get_category_title(object: Object, path: String) -> String:
-	printt(
-		path,
-		path.replace(CATEGORY_PREFIX, ""),
-		path.replace(CATEGORY_PREFIX, "").capitalize()
+	var title: String = ""
+	var category: Dictionary = _eval_from_object(object, path)
+	if _dict_has_string(category, CATEGORY_KEY_TITLE):
+		title = category[CATEGORY_KEY_TITLE]
+	if title.empty():
+		title = path.replace(CATEGORY_PREFIX, "").capitalize()
+	return title
+
+
+static func _eval_from_object(object: Object, path: String) -> Dictionary:
+	var dict: Dictionary = {}
+	var script: Script = object.get_script()
+	if script != null and not script.is_tool():
+		var code: String = script.source_code
+		code = code.right(code.find(path))
+		code = code.left(code.find("\n"))
+		var idx = code.find("jpInspector")
+		if idx >= 0:
+			code = code.right(code.find("jpInspector"))
+			dict = _eval_gdscript(code)
+	if dict.empty():
+		dict = object.get(path)
+	
+	return dict
+
+
+static func _eval_gdscript(code: String) -> Dictionary:
+	var gdscript := GDScript.new()
+	gdscript.source_code = """
+extends Reference
+
+func eval() -> Dictionary:
+	return %s
+	""" % code
+	gdscript.reload()
+	var instance: Reference = gdscript.new()
+	var value: Dictionary = instance.call("eval")
+	return value
+
+
+static func _dict_has_string(dict: Dictionary, key) -> bool:
+	return (
+		dict.has(key)
+		and typeof(dict[key]) == TYPE_STRING
+		and not dict[key].empty()
 	)
-	return path.replace(CATEGORY_PREFIX, "").capitalize()
--- addons/jp_inspector/tests/Test.gd
+++ addons/jp_inspector/tests/Test.gd
@@ -8,12 +8,16 @@ export var var_cat1_bool: bool
 export var var_cat1_int: int
 export var var_cat1_str: String
 
-export var _c_category_2: Dictionary
+export var _c_category_2: Dictionary = jpInspector.Category()
 export var var_cat2_bool: bool
 export var var_cat2_int: int
 export var var_cat2_str: String
 
 # Category with custom title
+export var _c_category_3: Dictionary = jpInspector.Category("My Category #3")
+export var var_cat3_bool: bool
+export var var_cat3_int: int
+export var var_cat3_str: String
 
 # Category with custom title and icon
 
